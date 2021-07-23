fb573212c387533deba0e783c45a8f6b37965c06
Eren I <erenss@gmail.com>
2021-07-23 14:36:41 +0300

    test2

--- CyrptoPascal.pas
+++ CyrptoPascal.pas
@@ -2,6 +2,28 @@ unit CyrptoPascal;
 
 interface
 
+uses
+  System.NetEncoding, SysUtils, Classes;
+
+type
+  TCyrptoPascal = class
+  public
+    function DecodeBase64(AText: string): string;
+    function EncodeBase64(AText: string): string;
+  end;
+
 implementation
+
+{ TCyrptoPascal }
+
+function TCyrptoPascal.DecodeBase64(AText: string): string;
+begin
+  DecodeBase64 := TNetEncoding.Base64.Decode(AText);
+end;
+
+function TCyrptoPascal.EncodeBase64(AText: string): string;
+begin
+  EncodeBase64 := TNetEncoding.Base64.Encode(AText);
+end;
 
 end.
